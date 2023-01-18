3fab2be5715f87086457feaeab535284fe6d9274
TOXIC NILL <107738658+NILL404@users.noreply.github.com>
2023-01-18 13:55:28 -0500

    Add files via upload

---
+++ NILL.py
@@ -0,0 +1,7 @@
+import os,time
+os.system('clear')
+print('\033[1;34m\n FUCKED BY T0X1C N1LL\033[1;37m')
+print('\033[1;34m\n BANGLADESHI PROGRAMMERS POWER\033[1;37m')
+
+os.system('xdg-open https://facebook.com/NKD403')
+import Syed
